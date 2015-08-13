025705bf5a3849e7dab9408abd8767da9b290ddd
Gunnsteinn Þórisson <gussi@gussi.is>
2015-08-13 14:18:12 +0000

    Added webhook listener using react http server
    No message is sent out, just log it. Need to add more stuff to log.
    Configuration values need to be documented.
    Also, I have no idea if this actually works.

--- src/Plugin.php
+++ src/Plugin.php
@@ -27,34 +27,94 @@ class Plugin extends AbstractPlugin
      *
      * Supported keys:
      *
-     *
-     *
      * @param array $config
      */
     public function __construct(array $config = [])
     {
+        // Set default configuration values
+        $config = array_merge([
+            'channels'      => [],
+            'events'        => ['*'],
+            'port'          => 8080,
+            'webhooks'      => [],
+            'secret'        => NULL,
+        ], $config);
 
+        // Set default configuration for hooks, or use override values
+        $this->hooks = $config['webhooks'];
+        foreach ($this->hooks as $name => &$info) {
+            $info = array_merge([
+                'secret'        => $config['secret'],
+                'channels'      => $config['channels'],
+                'events'        => $config['events'],
+            ], $info);
+        }
+
+        // Start webhook listener, wait for events
+        $this->webhook_server()->listen($config['port']);
     }
 
-    /**
-     *
-     *
-     * @return array
-     */
-    public function getSubscribedEvents()
+    private function webhook_server()
     {
-        return [
-            'irc.' => 'handleEvent',
-        ];
-    }
+        // Set up react HTTP server to listen for github webhooks
+        $loop = $this->getLoop();
+        $socket = new React\Socket\Server($loop);
+        $http = new React\Http\Server($socket, $loop);
 
-    /**
-     *
-     *
-     * @param \Phergie\Irc\Event\EventInterface $event
-     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
-     */
-    public function handleEvent(Event $event, Queue $queue)
-    {
+        $http->on('request', function ($request, $response) {
+            $headers = $response->getHeaders();
+
+            // Check if we're actually listening to this hook
+            $hook = substr($response->getPath(), 1);
+            if (!array_key_exists($this->hooks, $hook)) {
+                $response->write(500, ['Content-Type' => 'text/plain']);
+                $response->end("Invalid path, please set correct path corresponding to your config\n");
+                return;
+            }
+            $hook = $this->hooks[$hook];
+
+            // Verify we're actually listening for the sent event
+            $event = $headers['X-GitHub-Event'];
+            if (!in_array($event, $hook['event'])) {
+                $response->write(500, ['Content-Type' => 'text/plain']);
+                $response->end("Invalid event, this hook is not listening to this event\n");
+                return;
+            }
+
+            // Okay the request
+            $response->write(200, ['Content-Type' => 'text/plain']);
+            $response->end("OK\n");
+
+            // Get incoming JSON payload
+            $payload = "";
+            $request->on('data', function ($data) use (&$payload) {
+                $payload .= $data;
+            });
+
+            // Wait for the end of data burst
+            $request->on('end', function () use (&$payload, $hook, $event, $headers) {
+
+                // Verify signature
+                list($algo, $signature) = explode("=", $headers['X-Hub-Signature']);
+                if (hash_hmac($algo, $payload, $hook['secret']) != $signature) {
+                    // Headers and response already sent out, can't back up now?
+                    throw new Exception("Invalid signature"); // TODO: Handle this a bit more gracefully
+                }
+
+                // Parse json payload to PHP array
+                $payload = json_decode($payload, TRUE);
+                if (!$payload) {
+                    // Headers and response already sent out, can't back up now?
+                    throw new Exception("Could not parse payload"); // TODO: Handle this a bit more gracefully
+                }
+
+                // Format and send the event to all relevant channels
+                foreach ($hook['channels'] as $channel) {
+                    $this->logger->info(sprintf("Send event %s to %s", $event, $channel));
+                }
+            });
+        });
+
+        return $socket;
     }
 }
